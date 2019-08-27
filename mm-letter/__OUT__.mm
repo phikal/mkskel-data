07dfcf3c44d445581b34fafb9c24d06e22ded5a8
Philip K <philip@warpmail.net>
2019-08-27 15:18:11 +0200

    added mm-letter

---
+++ mm-letter/__OUT__.mm
@@ -0,0 +1,26 @@
+.IA "Addressee" "Addressee Title"
+Addressee Address
+.IE
+
+.WA "Writer" "Writer Title"
+Writer Address
+.WE
+
+.\" Specify Letter Options
+\" .LO CN					\" Confidential notation
+\" .LO RN 					\" Reference notation
+\" .LO AT					\" Attention
+.LO SA					\" Salutation
+\" .LO SJ					\" Subject line
+
+.\" Specify Letter Format
+.LT BL 					\" Blocked
+\" .LT SB					\" Semi-blocked
+\" .LT FB					\" Ful-blocked
+\" .LT SP					\" Simplified.
+
+.P
+.\" content
+
+.FC						\" Figure title
+.SG						\" Signature line
